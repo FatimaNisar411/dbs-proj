e472c0f8bcc83071a828b858a5a35ed1391332f1
shabarish <shabarishkera@gmail.com>
2023-05-23 06:30:44 +0530

    updates'

--- data/data.js
+++ data/data.js
@@ -1,33 +1,50 @@
-
 export const CATEGORIES = [
-  {
-    id:'c1',title:'Grocery',color: '#f54242'
-  },
-  {
-    id:'c2',title:'Rent',color: '#47fced'
-  },
-  {
-    id:'c3',title:'Travel',color: '#f5a442'
-  }, {
-    id:'c4',title:'Education',color: '#f5d142'
-  }, {
-    id:'c5',title:'Food',color: '#368dff'
-  }, {
-    id:'c6',title:'Funiture',color: '#41d95d'
-  }, {
-    id:'c7',title:'Cloths',color: '#9eecff'
-  }, {
-    id:'c8',title:'Others',color: '#ffc7ff'
-  } ]
-  //   new Category('c1', '', '#f5428d'),
-  //   new Category('c2', 'Quick & Easy', '#f54242'),
-  //   new Category('c3', 'Hamburgers', '#f5a442'),
-  //   new Category('c4', 'German', '#f5d142'),
-  //   new Category('c5', 'Light & Lovely', '#368dff'),
-  //   new Category('c6', 'Exotic', '#41d95d'),
-  //   new Category('c7', 'Breakfast', '#9eecff'),
-  //   new Category('c8', 'Asian', '#b9ffb0'),
-  //   new Category('c9', 'French', '#ffc7ff'),
-  //   new Category('c10', 'Summer', '#47fced')
-  // ];
-  export default CATEGORIES;
+    {
+        id: 'c1',
+        title: 'Grocery',
+        color: '#f54242'
+    },
+    {
+        id: 'c2',
+        title: 'Rent',
+        color: '#47fced'
+    },
+    {
+        id: 'c3',
+        title: 'Travel',
+        color: '#f5a442'
+    },
+    {
+        id: 'c4',
+        title: 'Education',
+        color: '#f5d142'
+    }, {
+        id: 'c5',
+        title: 'Food',
+        color: '#368dff'
+    }, {
+        id: 'c6',
+        title: 'Funiture',
+        color: '#41d95d'
+    }, {
+        id: 'c7',
+        title: 'Cloths',
+        color: '#9eecff'
+    }, {
+        id: 'c8',
+        title: 'Others',
+        color: '#ffc7ff'
+    }
+]
+// new Category('c1', '', '#f5428d'),
+// new Category('c2', 'Quick & Easy', '#f54242'),
+// new Category('c3', 'Hamburgers', '#f5a442'),
+// new Category('c4', 'German', '#f5d142'),
+// new Category('c5', 'Light & Lovely', '#368dff'),
+// new Category('c6', 'Exotic', '#41d95d'),
+// new Category('c7', 'Breakfast', '#9eecff'),
+// new Category('c8', 'Asian', '#b9ffb0'),
+// new Category('c9', 'French', '#ffc7ff'),
+// new Category('c10', 'Summer', '#47fced')
+// ];
+export default CATEGORIES;
