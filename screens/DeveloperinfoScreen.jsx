61d2de74680726b4600689f933cee03b2f5ac9b0
shabarish <shabarishkera@gmail.com>
2023-05-26 22:56:30 +0530

    updates

--- screens/DeveloperinfoScreen.jsx
+++ screens/DeveloperinfoScreen.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import { StyleSheet,View,Text} from 'react-native'
+import { GlobalStyles } from '../constants/constant'
 export default function DeveloperinfoScreen() {
+  
   return (
   <>
-  
+  <View style={styles.container}>
+<Text style={styles.title}>Developed By :</Text>
+<Text style={styles.text}>SHABARISH K :</Text>
+
+  </View>
   
   
   
@@ -12,3 +18,19 @@ export default function DeveloperinfoScreen() {
   </>
   )
 }
+const styles=StyleSheet.create({
+container:{
+flex:1,
+justifyContent:'center',
+alignItems:'center',
+backgroundColor:GlobalStyles.colors.back,
+
+},
+title:{fontFamily:'monospace',
+fontWeight:'bold'
+},
+text:{
+  fontWeight:'300',
+}
+
+})
